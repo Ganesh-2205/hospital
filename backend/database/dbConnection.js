260fafe4ffd656cbf7a98fe80bfbc740a39dca76
Ganesh Sharma <ganesh220503@gmail.com>
2024-06-28 12:48:15 +0530

    adding files to the repo

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,14 +1,28 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-export const dbConnection = () => {
-  mongoose
-    .connect(process.env.MONGO_URI, {
-      dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM",
-    })
-    .then(() => {
-      console.log("Connected to database!");
-    })
-    .catch((err) => {
-      console.log("Some error occured while connecting to database:", err);
-    });
-};
+// export const dbConnection = () => {
+//   mongoose
+//     .connect(process.env.MONGO_URI, {
+//       dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM",
+//     })
+//     .then(() => {   
+//       console.log("Connected to database!");
+//     })
+//     .catch((err) => {
+//       console.log("Some error occured while connecting to database:", err);
+//     });
+    
+// };
+import mongoose from 'mongoose';
+
+export const dbConnection=async()=>{
+    try{
+        const conn=await mongoose.connect("mongodb+srv://ganesh:ganeshmongo123@cluster0.qnoxxqu.mongodb.net/MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM");
+        // console.log(Connected to MongoDb ${conn.connection.host});
+    }
+    catch(error){
+        console.log(`Error in MongoDb is ${error}`)
+    }
+}
+ 
+ 
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -72,11 +72,11 @@ const Footer = () => {
             </div>
             <div>
               <MdEmail />
-              <span>zeelab@gmail.com</span>
+              <span>ganesh@gmail.com</span>
             </div>
             <div>
               <FaLocationArrow />
-              <span>Karachi, Pakistan</span>
+              <span>Rohtak , Haryana</span>
             </div>
           </div>
         </div>
